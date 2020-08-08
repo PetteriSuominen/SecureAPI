171bc7d6895c785824220e39ec728aac5655f69b
Petteri Suominen <petteri.suominen12@gmail.com>
2020-08-08 22:54:57 +0300

    Created reusable button component and styled it

---
+++ client/src/components/common/Button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: #00bfff;
+  color: white;
+  border: 2px solid #0080ff;
+  border-radius: 3px;
+  padding: 0.25rem 1rem;
+  cursor: pointer;
+
+  &:hover {
+    color: #00bfff;
+    background: white;
+  }
+`;
+
+export default Button;
